5802ac5e60969dbb1fc6db670f71a0aa2dce97e8
SovietTovarishch <07MOrlov@gmail.com>
2026-03-06 10:10:20 +0300

    Create Category.cs

---
+++ BOKIC3/Category.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Category
+{
+    string name;
+    int points;
+    Question[] questions;
+    bool is_finished;
+    bool is_active;
+
+
+    void showInfo()
+    {
+
+    }
+    void nextQuestion()
+    {
+
+    }
+    void previousQuestion()
+    {
+
+    }
+
+    void end()
+    {
+
+    }
+
+    void addQuestion()
+    {
+
+    }
+
+    void removeQuestion(int index)
+    {
+
+    }
+    int LoadFromFile()
+    {
+        
+        return 0;
+    }
+
+    int SaveInFile()
+    {
+
+        return 0;
+    }
+
+}
+
