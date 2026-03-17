2f68e00510b3f31407af5f5e84c445dffc16344c
SovietTovarishch <07MOrlov@gmail.com>
2026-03-17 22:32:47 +0300

    Прогресс по колонке Категории и вопросы

--- BOKIC3/Category.cs
+++ BOKIC3/Category.cs
@@ -2,7 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 
-class Category
+partial class Question
+{
+    public string content;
+    public string answer;
+    public bool correct;
+    public bool is_resolved;
+    public int point;
+
+    public int countPoints()
+    {
+        if (!correct) return 0;
+        else return point;
+    }
+
+    public Question(string content, string answer, int point)
+    {
+        this.content = content;
+        this.answer = answer;
+        correct = false;
+        is_resolved = false;
+        this.point = point;
+    }
+}
+partial class Category
 {
     string name;
     int points;
@@ -13,41 +36,45 @@ class Category
 
     void showInfo()
     {
-
-    }
-    void nextQuestion()
-    {
-
-    }
-    void previousQuestion()
-    {
-
+        Console.WriteLine(name);
+        Console.WriteLine(questions.Count + " Вопросов");
     }
 
-    void end()
+    void LoadFromFile(string filename)
     {
 
+        foreach (string line in File.ReadAllLines($"{filename}.txt"))
+        {
+            string[] parts = line.Split(':');
+            if (parts.Length != 3) continue; // защита от ошибок
+
+            questions.Add(new Question(parts[0], parts[1], int.Parse(parts[2])));
+        }
     }
 
-    void addQuestion()
+    void SaveInFile()
     {
-
-    }
-
-    void removeQuestion(int index)
-    {
-
-    }
-    int LoadFromFile()
-    {
-        
-        return 0;
-    }
-
-    int SaveInFile()
-    {
-
-        return 0;
+        foreach (Question question in questions)
+        {
+            
+            if (File.ReadAllLines($"{name}.txt").Contains(question.content)) {
+                string[] f = File.ReadAllLines($"{name}.txt");
+                for (int i = 0; i < f.Length; i++) {
+                    if (f[i].Contains(question.content))
+                    {
+                        f[i] = question.content + ":" + question.answer + ":" + question.point;
+                        break;
+                    }
+                    File.WriteAllText($"{name}.txt", "");
+                    File.WriteAllLines($"{name}.txt", f);
+                }
+            }
+            else
+            {
+                string[] a = { question.content + ":" + question.answer + ":" + question.point};
+                File.WriteAllLines($"{name}.txt", a);
+            }
+        }
     }
 
 }
